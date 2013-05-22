caa3faf5b3443a5ca68e76f4c7aa98c4a0ff2029
U-Dima-PC\Dima <Dima@Dima-PC.(none)>
2013-05-22 21:44:10 +0300

    Bot version 0.01.2

--- SkypeBot.py
+++ SkypeBot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import Skype4Py
 import time
@@ -9,17 +9,15 @@ import random
 from datetime import datetime
 
 currentTime = lambda : strftime("%X", gmtime())
-def Log(str): 
+def Log(str):
    logfile.write("%s: %s\n"%(currentTime(), str))
    logfile.flush()
-   
-   
+
 chatname = "#mika.rez/$kallagen;4dd32678e90d0523"
 testname = "#test454515/$avida.d3;f29bc3eff39e4bd4"
 admiral = 'kallagen'
 phrases = ["Гениально!", "Адмирал все правильно сказал!", "Я люблю Адмирала!", "Так точно!", "Конгениально!", "Истинно", "Одобряю", ]
-greetings = ["Приветствую, %s", "Здравствуй, %s", "Рад тебя видеть, %s" , "(wave), %s", "Доброе утро, %s"]
-users_to_greet = ["kallagen", "andrew.rumm", "ofelok", "hawkeye_atm", "asp_sn", "andrey.bembel", "inv1zt"]
+greetings = ["Приветствую, %s", "Здравствуй, %s", "Рад тебя видеть, %s" , "(wave), %s", "Доброе утро, %s", "Привет, %s"]
 known_users = {
    'hawkeye_atm' : 'Хоукай',
    'andrew.rumm' : 'Рэйз',
@@ -28,7 +26,12 @@ known_users = {
    'inv1zt' : 'Инвизич',
    'andrey.bembel' :  'Шизоид',
    'biruman_xpomocom' : 'Бируман',
-   'test454515' : 'Скайп бот'
+   'unit_0013' : 'Татц',
+   'trayich' : 'BlindedRageZ',
+   'test454515' : 'Скайп бот',
+   'browserofmetal' : 'Брауз',
+   'ofelok' : 'Офелок',
+   'mk777gif' : 'Ёж',
 }
 users_been_greeted = set()
 chat = None
@@ -41,17 +44,17 @@ class SkypeBot(object):
       self.skype.Attach()
       global chat
       chat = self.skype.Chat(chatname)
-  
+
   def AttachmentStatus(self, status):
     if status == Skype4Py.apiAttachAvailable:
       self.skype.Attach()
-      
+
   def OnlineStatus(self, user, status):
       if status == Skype4Py.cusOnline and user.Handle == admiral:
          message = '(*)'
          chat.SendMessage(message)
          Log(message)
-      
+
   def MessageStatus(self, msg, status):
    if msg.Chat.Name == chatname and status == Skype4Py.cmsReceived:
       sender = msg.Sender
@@ -66,7 +69,7 @@ class SkypeBot(object):
          users_been_greeted.add(sender.Handle)
       else:
          if msg.Sender.Handle == admiral:
-            if random.random() < 0.3:
+            if random.random() < 0.4:
                message = random.choice(phrases)
                chat.SendMessage(message.decode('cp1251'))
                Log(message)
@@ -75,7 +78,6 @@ if __name__ == "__main__":
   logfile = open('log.txt','a')
   Log('Log started')
   bot = SkypeBot()
-  
+
   while True:
     time.sleep(1.0)
-    
